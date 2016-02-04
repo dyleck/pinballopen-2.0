de6bac9aed570e505f1fd6230ed262e863983f71
Marcin Dylewski <marcin.dylewski@dylux.net>
2016-02-05 00:08:25 +0100

    Carousel autostart fix.

--- vendor/assets/javascripts/main.js
+++ vendor/assets/javascripts/main.js
@@ -1,5 +1,12 @@
 jQuery(function($) {
 
+	//#main-slider
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 8000
+		});
+	});
+
 	$( '.centered' ).each(function( e ) {
 		$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
 	});
