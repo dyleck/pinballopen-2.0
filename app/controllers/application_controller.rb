c1e6a7b4e64ca5517a55b5b554c7af3eef966147
Marcin Dylewski <marcin.dylewski@dylux.net>
2016-02-12 00:40:11 +0100

    Menu localized.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_locale_from_url
+
+  def set_locale_from_url
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      end
+    end
+  end
 end
