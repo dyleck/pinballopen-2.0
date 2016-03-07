070a0d79aeb213939d4723532a78224b8476f03a
Marcin Dylewski <marcin.dylewski@dylux.net>
2016-03-07 21:25:41 +0100

    Fix sff_price used only if available.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
   has_one :order_item
 
   def discount_if_sff_local_price(sff)
-    price = sff ? self.sff_price : self.price
+    price = (sff && !self.sff_price.nil? && self.sff_price > 0) ? self.sff_price : self.price
     case I18n.locale
       when :pl then price
       when :en then (price / 4.4).to_i + 1
