95fd7ca6f7f14e34b1febf94f80fece30a2fa035
Marcin Dylewski <marcin.dylewski@dylux.net>
2016-03-20 22:29:56 +0100

    Delete orphaned orders when new one is created.

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,15 +2,25 @@ module OrdersHelper
   def set_order
     if order_id = session[:order_id]
       if !(@current_order = Order.find_by(id: order_id))
-        @current_order = Order.create(user_id: session[:user_id])
-        session[:order_id] = @current_order.id
+        create_empty_order
+        remove_empty_orders
       end
     else
-      @current_order = Order.create(user_id: session[:user_id])
-      session[:order_id] = @current_order.id
+      create_empty_order
+      remove_empty_orders
     end
     if @current_order.locale != I18n.locale
       @current_order.recalc_local_prizes_of_items
     end
   end
+
+  private
+    def create_empty_order
+      @current_order = Order.create(user_id: session[:user_id])
+      session[:order_id] = @current_order.id
+    end
+
+    def remove_empty_orders
+      Order.where(payed: [nil, false], user_id: session[:user_id]).where.not(id: session[:order_id]).delete_all
+    end
 end
