98a41fa9388dc939bc8891aaf9f1854bd4e1f2b1
Marcin Dylewski <marcin.dylewski@dylux.net>
2016-03-20 18:32:20 +0100

    Added Orders partial on user's profile. Prevents buying one_timers across multiple orders.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,10 @@ module UsersHelper
   def is_activated?
     current_user.activated?
   end
+
+  def current_user
+    if session[:user_id]
+      User.find_by(id: session[:user_id])
+    end
+  end
 end
